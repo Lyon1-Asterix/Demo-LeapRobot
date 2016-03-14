4fcea36e099515a2eed82d057cbcb2c5543600ee
Ananasr <h.nsr69@gmail.com>
2016-03-14 20:49:10 +0100

    NSHIELD-M Fonctionel avec l'Arduino Mega

--- arduino/nxt/nxt.ino
+++ arduino/nxt/nxt.ino
@@ -2,12 +2,12 @@
 #include <NXShield.h>
 
 NXShield nxshield;
+int c = -1;
+long            rotations = 2;  // Define variable rotations and set
+// equal to 90
+char            str[40];
 
 void setup() {
-  long            rotations = 2;  // Define variable rotations and set
-  // equal to 90
-  char            str[40];
-
   Serial.begin(115200);
   delay(500);
 
@@ -23,32 +23,56 @@ void setup() {
   // reset motors.
   //
   nxshield.bank_a.motorReset();
-  // Drive motor 1 forward and backward for a specific number of
-  // rotations
-  delay(1000);
-  Serial.println("Bank A motors >>");
-  sprintf(str, "Motor 1 Forward %d Rotations", rotations);
-  Serial.println(str);
-  str[0] = '\0';
-  nxshield.bank_a.motorRunRotations(SH_Motor_1,
-                                    SH_Direction_Forward,
-                                    SH_Speed_Medium,
-                                    rotations,
-                                    SH_Completion_Wait_For,
-                                    SH_Next_Action_BrakeHold);
-  delay(1000);
-  sprintf(str, "Motor 1 Reverse %d Rotations", rotations);
-  Serial.println(str);
-  str[0] = '\0';
-  nxshield.bank_a.motorRunRotations(SH_Motor_1,
-                                    SH_Direction_Reverse,
-                                    SH_Speed_Medium,
-                                    rotations,
-                                    SH_Completion_Wait_For,
-                                    SH_Next_Action_BrakeHold);
+  nxshield.bank_b.motorReset();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    c = Serial.read();
+    switch (c) {
+      case 2 : // droite
+        nxshield.bank_a.motorRunUnlimited(SH_Motor_1,
+                                          SH_Direction_Reverse,
+                                          50);
+        nxshield.bank_b.motorRunUnlimited(SH_Motor_1,
+                                          SH_Direction_Forward,
+                                          50);
+        break;
+      case 3 : // gauche
+        nxshield.bank_a.motorRunUnlimited(SH_Motor_1,
+                                          SH_Direction_Forward,
+                                          50);
+        nxshield.bank_b.motorRunUnlimited(SH_Motor_1,
+                                          SH_Direction_Reverse,
+                                          50);
+        break;
+      case 4: // avance
+        Serial.println("Bank A motors >>");
+        sprintf(str, "Motor 1 Forward %d Rotations", rotations);
+        Serial.println(str);
+        nxshield.bank_a.motorRunUnlimited(SH_Motor_1,
+                                          SH_Direction_Reverse,
+                                          100);
+        nxshield.bank_b.motorRunUnlimited(SH_Motor_1,
+                                          SH_Direction_Reverse,
+                                          100);
+        break;
+      case 5: // recule
 
+        nxshield.bank_a.motorRunUnlimited(SH_Motor_1,
+                                          SH_Direction_Forward,
+                                          100);
+        nxshield.bank_b.motorRunUnlimited(SH_Motor_1,
+                                          SH_Direction_Forward,
+                                          100);
+        break;
+      case 6: // arrêt
+        nxshield.bank_a.motorStop(SH_Motor_1,
+                                  SH_Next_Action_Float);
+        nxshield.bank_b.motorStop(SH_Motor_1,
+                                  SH_Next_Action_Float);
+        break;
+    }
+  }
 }
